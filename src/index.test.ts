6db988df7f01231547e2710fff234f6a441d0019
Cory <c.donkin@elifesciences.org>
2021-06-09 10:45:04 +0100

    fix: prettier linting config

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,15 @@
-import { Server } from "http";
-import request from "supertest";
+import { Server } from 'http';
+import request from 'supertest';
 
-describe("server", () => {
-  let server: Server;
-  beforeEach(() => {
-    server = require("./index");
-  });
-  afterEach(() => {
-    server.close();
-  });
-  it("responds to /health request", (done) => {
-    request(server).get("/health").expect(200, done);
-  });
+describe('server', () => {
+    let server: Server;
+    beforeEach(() => {
+        server = require('./index');
+    });
+    afterEach(() => {
+        server.close();
+    });
+    it('responds to /health request', (done) => {
+        request(server).get('/health').expect(200, done);
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import express from "express";
+import express from 'express';
 
 //TODO: replace with injectable / importable config object
 const config = {
-  port: process.env.PORT || 4000,
+    port: process.env.PORT || 4000,
 };
 
-console.log("Starting server...");
+console.log('Starting server...');
 
 const app: express.Application = express();
-app.get("/health", (_, res) => res.sendStatus(200));
+app.get('/health', (_, res) => res.sendStatus(200));
 
 const server = app.listen(config.port, () => {
-  // Make sure the application cleanly shuts down on SIGINT
-  process.on("SIGINT", terminate);
-  process.on("SIGTERM", terminate);
+    // Make sure the application cleanly shuts down on SIGINT
+    process.on('SIGINT', terminate);
+    process.on('SIGTERM', terminate);
 
-  console.log(`Server listening on port ${config.port}`);
+    console.log(`Server listening on port ${config.port}`);
 });
 
 // Cleanly shuts down the application
 function terminate(): void {
-  console.log(`Shutting down...`);
-  if (server) {
-    server.close(() => {
-      process.exit(0);
-    });
-  } else {
-    process.exit(0);
-  }
+    console.log(`Shutting down...`);
+    if (server) {
+        server.close(() => {
+            process.exit(0);
+        });
+    } else {
+        process.exit(0);
+    }
 }
 
 module.exports = server;
