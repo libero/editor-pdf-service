49415984767c590ca7a42693a2f03198e3896f01
Cory <c.donkin@elifesciences.org>
2021-06-18 10:33:56 +0100

    feat: call off to start job endpoint

--- src/services/generate.test.ts
+++ src/services/generate.test.ts
@@ -8,13 +8,50 @@ describe('GenerateService', () => {
     });
 
     describe('startJob', () => {
-        it('throws NotFound error if article not found', async () => {
-            (fetch as unknown as jest.Mock).mockImplementationOnce(() => ({ status: 404 }));
-            await expect(GenerateService({}).startJob('11111')).rejects.toThrow('NotFound');
+        beforeEach(() => {
+            (fetch as unknown as jest.Mock).mockReset();
         });
-        it('doesn\t throw when article exists', async () => {
-            (fetch as unknown as jest.Mock).mockImplementationOnce(() => ({ status: 200 }));
-            await expect(GenerateService({}).startJob('11111')).resolves.toBeUndefined();
+        it('throws NotFound error if article not found', async () => {
+            (fetch as unknown as jest.Mock).mockImplementation((fetchURL: string) => {
+                if (fetchURL === 'someUrl/11111') return { status: 404 };
+                return { json: () => ({ status: { code: 200 }, message: { jobid: 'someJobId' } }) };
+            });
+            await expect(GenerateService({ articleStorePath: 'someUrl/' }).startJob('11111')).rejects.toThrow(
+                'NotFound',
+            );
+        });
+        it('doesn\t throw when article exists and returns jobid', async () => {
+            (fetch as unknown as jest.Mock).mockImplementation((fetchURL: string) => {
+                if (fetchURL === 'someUrl/11111') return { status: 200 };
+                return { json: () => ({ status: { code: 200 }, message: { jobid: 'someJobId' } }) };
+            });
+            await expect(GenerateService({ articleStorePath: 'someUrl/' }).startJob('11111')).resolves.toBe(
+                'someJobId',
+            );
+        });
+        it("throws when job start request doesn't return 200 status", async () => {
+            (fetch as unknown as jest.Mock).mockImplementation((fetchURL: string) => {
+                if (fetchURL === 'someUrl/11111') return { status: 200 };
+                return { json: () => ({ status: { code: 500, message: 'Something went worng!' } }) };
+            });
+            await expect(GenerateService({ articleStorePath: 'someUrl/' }).startJob('11111')).rejects.toThrow(
+                'Start generation job failed - code: 500 message: Something went worng!',
+            );
+        });
+        it('sends start job request with expected encodedform values', async () => {
+            (fetch as unknown as jest.Mock).mockImplementation((fetchURL: string) => {
+                if (fetchURL === 'someUrl/11111') return { status: 200 };
+                return { json: () => ({ status: { code: 200 }, message: { jobid: 'someJobId' } }) };
+            });
+            await GenerateService({
+                articleStorePath: 'someUrl/',
+                generationJobResourcesPath: 'generationJobResourcesPath/',
+                generaionJobApiKey: 'someApiKey',
+            }).startJob('11111');
+            expect((fetch as unknown as jest.Mock).mock.calls[1][1].body).toBeInstanceOf(URLSearchParams);
+            expect((fetch as unknown as jest.Mock).mock.calls[1][1].body.toString()).toBe(
+                'client=elife-libero&id=elife.11111&idType=doi&processType=InDesignSetter&project=elife-libero&proof=online&proofingVersion=v2.0&siteName=generationJobResourcesPath%2F11111&proofingEngine=InDesignSetter&apiKey=someApiKey',
+            );
         });
     });
 });
